4dd1f8b4cb6f7a1080c20618196bc68ff6278d8b
heuzin <matheusqs.arq@gmail.com>
2020-08-07 18:52:11 -0400

    06_tuples

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -36,6 +36,11 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 # YOUR CODE HERE
 
+def print_tuple(tupple):
+    for i in tupple:
+        print(i)
+
+
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
